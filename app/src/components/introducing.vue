cd2a1f56131d72f34310ad1702c8b8f47322b1ba
TavaresDylan <tavares.dylan03@gmail.com>
2023-01-26 13:45:10 +0100

    feat: vuejs views & components | router

---
+++ app/src/components/introducing.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h2 class="text-uppercase"><span class="font-weight-bold">Fight</span>girl</h2>
+    <h3 class="text-uppercase text-h1">Sarah Time</h3>
+  </div>
+</template>
