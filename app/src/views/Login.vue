f56c8a47388d6ecea63aa4e1b8de62075e82c4fc
TavaresDylan <tavares.dylan03@gmail.com>
2023-04-26 10:06:01 +0200

    refactor: simple login page

--- app/src/views/Login.vue
+++ app/src/views/Login.vue
@@ -1,50 +1,71 @@
 <template>
   <v-container>
     <h1 class="mt-4">Connexion</h1>
-    <Form>
-      <template v-slot:card-content>
-        <v-label color="black">E-mail :</v-label>
-        <v-text-field
-          class="mb-2"
-          v-model="mail"
-          variant="text"
-          hide-details="auto"
-          :rules="isNotEmpty"
-          dense
-          color="black"
-          placeholder="Votre adresse e-mail"
-        ></v-text-field>
-        <v-label color="black">Mot de passe :</v-label>
-        <v-text-field
-          class="mb-2"
-          v-model="password"
-          hide-details="auto"
-          variant="text"
-          type="password"
-          :rules="isNotEmpty"
-          dense
-          required
-          placeholder="Votre mot de passe"
-        ></v-text-field>
-      </template>
-
-      <template v-slot:form-actions>
-        <v-checkbox v-model="rememberMe" label="Se souvenir de moi"></v-checkbox>
-        <p class="text-center">
-          <router-link to="/mot-de-passe-oublie"
-            >Mot de passe oublié ?</router-link
+    <v-row id="form-container">
+      <div id="bar"></div>
+      <v-spacer></v-spacer>
+      <v-col cols="10" md="6" lg="4">
+        <v-form @submit.prevent>
+          <v-card
+            id="card"
+            outlined
+            class="px-4 py-1 border border-dark rounded rounded-lg"
           >
-          / <router-link to="/inscription">S'inscrire</router-link>
-        </p>
-        <Button class="text-center mt-4" btnText="Se connecter"></Button>
-      </template>
-    </Form>
+            <v-label color="black">E-mail :</v-label>
+            <v-text-field
+              class="mb-2"
+              v-model="mail"
+              variant="text"
+              hide-details="auto"
+              :rules="isNotEmpty"
+              dense
+              color="black"
+              placeholder="Votre adresse e-mail"
+            ></v-text-field>
+            <v-label color="black">Mot de passe :</v-label>
+            <v-text-field
+              class="mb-2"
+              v-model="password"
+              hide-details="auto"
+              variant="text"
+              type="password"
+              :rules="isNotEmpty"
+              dense
+              required
+              placeholder="Votre mot de passe"
+            ></v-text-field>
+          </v-card>
+
+          <v-checkbox
+            v-model="rememberMe"
+            label="Se souvenir de moi"
+          ></v-checkbox>
+          <p class="text-center">
+            <router-link to="/mot-de-passe-oublie"
+              >Mot de passe oublié ?</router-link
+            >
+            / <router-link to="/inscription">S'inscrire</router-link>
+          </p>
+          <div class="text-center mt-4">
+            <v-btn
+              @click="submit()"
+              size="x-large"
+              prepend-icon="mdi-arrow-right"
+              color="primary"
+              class="rounded rounded-xl"
+              id="custom-btn"
+            >
+              Se Connecter
+            </v-btn>
+          </div>
+        </v-form>
+      </v-col>
+      <v-spacer></v-spacer>
+    </v-row>
   </v-container>
 </template>
 
 <script lang="ts" setup>
-import Button from "../components/Button.vue";
-import Form from "../components/Form.vue";
 import { ref } from "vue";
 
 const mail = ref("");
@@ -52,6 +73,10 @@ const password = ref("");
 const rememberMe = ref(false);
 
 const isNotEmpty = [(v: string) => !!v || "Ce champ est requis"];
+
+function submit() {
+  console.log("submited");
+}
 </script>
 
 <style lang="css" scoped>
@@ -63,5 +88,29 @@ a {
 a:hover {
   color: #0000ff;
   transition: ease-in 0.1s;
+}
+#custom-btn {
+  border: 2px solid black;
+  font-weight: bold;
+}
+
+#form-container {
+  position: relative;
+  overflow: visible;
+  z-index: 1000;
+}
+#bar {
+  position: absolute;
+  width: 85vw;
+  height: 3px;
+  background: black;
+  border-radius: 1px;
+  top: 35px;
+  left: 50%;
+  transform: translate(-50%, 0);
+  z-index: 0;
+}
+#card {
+  border: 2px solid black !important;
 }
 </style>
