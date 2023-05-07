08b4a5c810d3b175714aa07411d45e70361acdc5
TavaresDylan <tavares.dylan03@gmail.com>
2023-05-08 00:11:01 +0200

    chore: deployment of VueJS app (first try)

--- app/vite.config.js
+++ app/vite.config.js
@@ -5,6 +5,10 @@ import { fileURLToPath, URL } from "url";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/",
+  build: {
+    outDir: '../dist', // this line place index.html in the public folder
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
