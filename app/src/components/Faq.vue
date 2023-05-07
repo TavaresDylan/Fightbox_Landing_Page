4c43c944814f1515d9dd443c1fd5a13b1d6f036e
TavaresDylan <tavares.dylan03@gmail.com>
2023-05-07 23:36:39 +0200

    refactor: adding TS type for ref

--- app/src/components/Faq.vue
+++ app/src/components/Faq.vue
@@ -1,9 +1,5 @@
 <template>
-  <v-container
-    id="container-faq"
-    fluid
-    class="bg-white mb-n6 rounded-t-xl"
-  >
+  <v-container id="container-faq" fluid class="bg-white mb-n6 rounded-t-xl">
     <v-container>
       <span class="text-h6 font-weight-bold">FAQ</span>
       <v-expansion-panels class="my-4" variant="accordion">
@@ -25,28 +21,33 @@
 <script lang="ts" setup>
 import { ref } from "vue";
 
-const faqContent = ref([
+type TfaqContent = {
+  title: string;
+  text: string;
+};
+
+const faqContent = ref<Array<TfaqContent>>([
   {
     title: "Que comprend votre abonnement ?",
     text: "Lorem ipsum dolor sit amet, consect etur adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis partu rient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.",
   },
   {
     title: "Quelles différence y a-t-il entre les abonnements ?",
-    text: "Hello ceci est un texte",
+    text: "Lorem ipsum dolor sit amet, consect etur adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis partu rient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.",
   },
   {
     title:
       "Comment adhérer à une autre formule lorsque j'ai un abonnement en cours (par exemple lorsqu'il y a des offres promotionnelles) ?",
-    text: "Hello ceci est un texte",
+    text: "Lorem ipsum dolor sit amet, consect etur adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis partu rient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.",
   },
   {
     title: "Comment effectuer la résiliation d'abonnement ?",
-    text: "Hello ceci est un texte",
+    text: "Lorem ipsum dolor sit amet, consect etur adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis partu rient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.",
   },
   {
     title:
       "J'ai payé l'abonnement mais il n'est pas actif, que dois-je faire ?",
-    text: "Hello ceci est un texte",
+    text: "Lorem ipsum dolor sit amet, consect etur adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis partu rient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.",
   },
 ]);
 </script>
