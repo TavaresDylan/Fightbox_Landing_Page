c8d9a0f53c1e62e3074ab5d8579b8171ead58ff1
Tavares Dylan <tavares.dylan03@gmail.com>
2023-05-08 22:43:48 +0200

    feat: our products (#37)
    * feat: route and link in nav added
    
    * feat: product cards
    
    * refactor: icon of our-products page changed in mobile nav

---
+++ app/src/components/TermCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card class="card rounded-lg" height="240px">
+    <div class="top d-flex align-center">
+      <v-icon class="ml-2" size="x-small" icon="mdi-circle-outline" color="white" />
+      <v-icon size="x-small" icon="mdi-circle-outline" color="white" />
+      <v-icon size="x-small" icon="mdi-circle-outline" color="white" />
+    </div>
+    <slot></slot>
+  </v-card>
+</template>
+
+<style lang="css" scoped>
+.card {
+  border: 2px solid black;
+}
+.top {
+  width: 100%;
+  height: 33px;
+  background-color: black;
+}
+</style>
